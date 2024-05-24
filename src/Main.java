cb2be9ad450e1ae49a0ec4d2fc8a6440056f62d8
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-05-24 23:58:43 +0300

    Remove theming due to UI bugs

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 import gui.ProxyGui;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignore) {
-        }
         new ProxyGui();
     }
 }
